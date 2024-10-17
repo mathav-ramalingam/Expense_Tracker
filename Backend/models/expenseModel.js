4836c52a9894250962ac24ea8b16162f7c65f3c3
mathav-ramalingam <mathavramalingam1608@gmail.com>
2024-10-17 23:34:45 +0530

    Backend Modified

--- Backend/models/expenseModel.js
+++ Backend/models/expenseModel.js
@@ -1,38 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ExpenseSchema = new mongoose.Schema({
+const ExpenseSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true, 
-        trim: true,
-        maxLength: 50
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
     },
     amount: {
-        type: Number,
-        required: true,
-        maxLength: 20,
-        trim: true
+      type: Number,
+      required: true,
+      maxLength: 20,
+      trim: true,
     },
     type: {
-        type: String,
-        default: "income"
+      type: String,
+      default: "expense",
     },
     date: {
-        type: Date,
-        required: true,
-        trim: true
+      type: Date,
+      required: true,
+      trim: true,
     },
     category: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     description: {
-        type: String,
-        required: true,
-        maxLength: 200,          
-        trim: true
-    }
-}, { timestamps: true });  
+      type: String,
+      required: true,
+      maxLength: 200,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Expense', ExpenseSchema);
+module.exports = mongoose.model("Expense", ExpenseSchema);
