cdc6b78a555f53af6c3aceb8f03b92c7a2d4e959
mathav-ramalingam <mathavramalingam1608@gmail.com>
2024-10-17 20:32:04 +0530

    Navbar Created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,35 @@
+import styled from "styled-components";
+import bg from "./img/bg.png";
+import { MainLayout } from "./styles/Layouts";
+import Orb from "./Components/Orb/Orb";
+import Navigation from "./Components/Navigation/Navigation";
+import React, { useState, useMemo } from "react";
 
 function App() {
+  const [active, setActive] = React.useState(1);
+
+  const orbMemo = useMemo(() => {
+    return <Orb />;
+  }, []);
+
   return (
-    <div className="App">
-      
-    </div>
+    <AppStyled bg={bg} className="App">
+      {orbMemo}
+      <MainLayout>
+        <Navigation active={active} setActive={setActive} />
+        <main></main>
+      </MainLayout>
+    </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${(props) => props.bg});
+  position: relative;
+  main {
+    flex: 1;
+  }
+`;
 
 export default App;
