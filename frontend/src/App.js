77fe78a9b8ba6c84155495a7e2cbf4874bd702e1
mathav-ramalingam <mathavramalingam1608@gmail.com>
2024-10-23 00:41:09 +0530

    Routing Modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,31 @@
 import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import bg from "./img/bg.png";
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./Components/Orb/Orb";
 import Navigation from "./Components/Navigation/Navigation";
 import React, { useState, useMemo } from "react";
-import Dashboard from "./Components/Dashboard/Dashboard";
+// import Dashboard from "./Components/Dashboard/Dashboard";
 import Income from "./Components/Incomes/Incomes";
 import Expenses from "./Components/Expenses/Expenses";
 import { useGlobalContext } from "./context/globalContext";
 import ViewTransaction from "./Components/ViewTransaction/ViewTransaction";
+import Login from "./Components/Auth/Login";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Register from "./Components/Auth/Register";
+import Main from "./Main";
 
 function App() {
-  const [active, setActive] = React.useState(1);
-
-  const global = useGlobalContext()
-  console.log(global);
-
-  
-  const displayData = () => {
-    switch(active){
-      case 1:
-        return <Dashboard />
-      case 2:
-        return <ViewTransaction />
-      case 3:
-        return <Income />
-      case 4: 
-        return <Expenses />
-      default: 
-        return <Dashboard />
-    }
-  }
-
-  const orbMemo = useMemo(() => {
-    return <Orb />;
-  }, []);
-
   return (
-    <AppStyled bg={bg} className="App">
-      {orbMemo}
-      <MainLayout>
-        <Navigation active={active} setActive={setActive} />
-        <main>
-          {displayData()}
-        </main>
-      </MainLayout>
-    </AppStyled>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Main />} />
+          <Route path="/Register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
@@ -53,14 +33,14 @@ const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${(props) => props.bg});
   position: relative;
-  main{
+  main {
     flex: 1;
     background: rgba(252, 226, 249, 0.78);
-    border: 3px solid #FFFFFF;
+    border: 3px solid #ffffff;
     backdrop-filter: blur(4.5px);
     border-radius: 32px;
     overflow-x: hidden;
-    &::-webkit-scrollbar{
+    &::-webkit-scrollbar {
       width: 0;
     }
   }
