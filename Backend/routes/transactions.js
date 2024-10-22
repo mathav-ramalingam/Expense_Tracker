f790dc033aa5b40d6790e5b834df1d9961f8b5e8
mathav-ramalingam <mathavramalingam1608@gmail.com>
2024-10-23 00:38:02 +0530

    Register Backend created

--- Backend/routes/transactions.js
+++ Backend/routes/transactions.js
@@ -1,14 +1,26 @@
-const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
-const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
+const {
+  addExpense,
+  getExpense,
+  deleteExpense,
+} = require("../controllers/expense");
+const {
+  addIncome,
+  getIncomes,
+  deleteIncome,
+} = require("../controllers/income");
+const { authdetails } = require("../controllers/auth.control");
+const { authlogin } = require("../controllers/auth.control");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/add-income', addIncome)
-    .get('/get-incomes', getIncomes)
-    .delete('/delete-income/:id', deleteIncome)
-    .post('/add-expense', addExpense)
-    .get('/get-expense', getExpense)
-    .delete('/delete-expense/:id', deleteExpense)
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIncomes)
+  .delete("/delete-income/:id", deleteIncome)
+  .post("/add-expense", addExpense)
+  .get("/get-expense", getExpense)
+  .delete("/delete-expense/:id", deleteExpense)
+  .post("/data-api", authdetails)
+  .post("/data-login", authlogin);
 
-
-module.exports = router   
+module.exports = router;
