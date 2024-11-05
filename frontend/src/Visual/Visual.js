7e0140df659e3b66e03e448e47d5befcde1db3de
mathav-ramalingam <mathavramalingam1608@gmail.com>
2024-11-05 22:13:34 +0530

    Visualization component is added

---
+++ frontend/src/Visual/Visual.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PieChart from '../Components/Chart/PieChart'
+import BarChart from '../Components/Chart/BarChart'
+import DownloadReport from '../Components/DownloadReport/DownloadReport'
+
+
+const Visual = () => {
+  return (
+    <>
+    <DownloadReport />
+    <PieChart />
+    <BarChart />
+
+    </>
+  )
+}
+
+export default Visual
